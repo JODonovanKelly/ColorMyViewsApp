fb8f767c3e1d90fbb7c949548a31d51aac229a50
JODonovanKelly <JODonovanKelly@gmail.com>
2022-07-08 07:32:13 +0100

    Finished the app with the jankiest, ugliest code I've ever made, please help
    Signed-off-by: JODonovanKelly <JODonovanKelly@gmail.com>

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,7 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +21,13 @@ class MainActivity : AppCompatActivity() {
                     findViewById(R.id.box_three_text),
                     findViewById(R.id.box_four_text),
                     findViewById(R.id.box_five_text),
-                    findViewById(R.id.constraint_layout))
+                    findViewById(R.id.constraint_layout),
+                    findViewById(R.id.red_button),
+                    findViewById(R.id.yellow_button),
+                    findViewById(R.id.green_button)
+
+
+                )
 
         for (item in clickableViews){
             item.setOnClickListener { makeColored(it)}
@@ -38,6 +44,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<TextView>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
